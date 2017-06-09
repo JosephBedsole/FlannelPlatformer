27e93be1078a44bd05439c82782ae8675a0a80b2
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-06-08 22:15:08 -0500

    init commit

---
+++ Assets/Scripts/Editor/TileMapEditor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapEditor : Editor {
+
+    public static int tile = 0;
+
+    public TileMap tileMap
+    {
+        get { return target as TileMap;  }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        tile = EditorGUILayout.IntField("Tile", tile);
+
+    }
+
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
+
+        if (!e.isMouse) return;
+
+        int id = GUIUtility.GetControlID(FocusType.Passive);
+
+        Vector3 pos = MouseToWorld();
+        int x = (int)pos.x;
+        int y = (int)pos.y;
+        if (x > 0 && x < tileMap.width && y >= 0 && y < tileMap.height)
+        {
+            if (e.type == EventType.MouseDown || e.type == EventType.MouseDrag)
+            {
+                if (e.button == 0)
+                {
+                    tileMap.SetTile(x, y, tile);
+                }
+                else if (e.button == 1)
+                {
+                    tileMap.SetTile(x, y, -1);
+                }
+            }
+            e.Use();
+            GUIUtility.hotControl = id;
+        }
+    }
+
+    Vector3 MouseToWorld ()
+    {
+        Event e = Event.current;
+
+        Ray ray = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+        Plane plane = new Plane(Vector3.forward, tileMap.transform.position);
+        float dist = 0;
+
+        if (plane.Raycast(ray, out dist))
+        {
+            
+            return ray.GetPoint(dist) - tileMap.transform.position;
+          
+        }
+        return Vector3.zero;
+    }
+
+}
---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class TileMap : MonoBehaviour {
+
+    public Texture2D texture;
+    public int tileWidth;
+    public int tileHeight;
+    public int tileMargin;
+    public int tileSpacing;
+
+    public int height = 10;
+    public int width = 10;
+
+    [HideInInspector] public int[] tileArray;
+
+    public void SetTile(int x, int y, int tile)
+    {
+        if (tileArray == null || tileArray.Length != (width * height))
+        {
+            tileArray = new int[width * height];
+        }
+        tileArray[x + y * width] = tile;
+        SetupMesh();
+    }
+    
+    Vector2[] LineUVs (int tile)
+    { // index = 4;
+        int columns = (texture.width / tileWidth);
+        int rows = tile / columns;
+        int cols = tile % columns;
+
+        float uvX = (float)(tileMargin + cols * (tileWidth + tileSpacing)) / (float)texture.width;
+        float uvY = (float)(tileMargin + rows * (tileHeight + tileSpacing)) / (float)texture.height;
+        float w = ((float)tileWidth / (float)texture.width);
+        float h = ((float)tileHeight / (float)texture.height);
+
+        return new Vector2[]
+        {
+            new Vector2(uvX, uvY),
+            new Vector2(uvX, uvY + h),
+            new Vector2(uvX + w, uvY + h),
+            new Vector2(uvX + h, uvY)
+        };
+    }
+
+    void SetupMesh()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        List<Vector3> verts = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<Color> colors = new List<Color>();
+        List<int> tris = new List<int>();
+        int index = 0;
+
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                int tile = tileArray[x + y * width];
+                if (tile < 0) continue;
+
+                Vector3 pos = new Vector3(x, y, 0);
+                verts.AddRange(new Vector3[]
+                {
+                pos,
+                pos + Vector3.up,
+                pos + new Vector3(1, 1, 0),
+                pos + new Vector3(1, 0, 0)
+                });
+
+                uvs.AddRange(LineUVs(tile));
+
+                colors.AddRange(new Color[]
+                {
+                Color.white,
+                Color.white,
+                Color.white,
+                Color.white,
+                });
+
+                // Assign tris last or you will break things!
+                
+                tris.AddRange(new int[]
+                {
+                index, index + 1, index + 2,
+                index, index + 2, index + 3
+                });
+                index += 4;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.name = "ProcMesh";
+        mesh.vertices = verts.ToArray();
+        mesh.uv = uvs.ToArray();
+        mesh.colors = colors.ToArray();
+        mesh.triangles = tris.ToArray();
+
+
+        meshFilter.sharedMesh = mesh;
+
+    }
+}
